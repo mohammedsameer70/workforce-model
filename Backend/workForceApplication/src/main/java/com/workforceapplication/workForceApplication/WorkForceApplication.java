7261dd5c8ad358515fee171da7fe934ee89860ee
Mohammed Sameer <ifsssff7@gmail.com>
2026-07-26 20:38:57 +0530

    feat: Implement of springBoot backend services

---
+++ Backend/workForceApplication/src/main/java/com/workforceapplication/workForceApplication/WorkForceApplication.java
@@ -0,0 +1,13 @@
+package com.workforceapplication.workForceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkForceApplication.class, args);
+	}
+
+}
---
+++ Backend/workForceApplication/src/test/java/com/workforceapplication/workForceApplication/WorkForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workforceapplication.workForceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
